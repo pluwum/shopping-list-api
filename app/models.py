e1ae9c86ef0ea0fc50527e169a830db642148430
Luwum Patrick Alvin <luwyxx@gmail.com>
2017-11-21 12:55:48 +0300

    Reduce the token expiry time

--- app/models.py
+++ app/models.py
@@ -47,7 +47,7 @@ class User(db.Model):
         try:
             # Set up a time based payload
             payload = {
-                'exp': datetime.utcnow() + timedelta(minutes=52560000000000),
+                'exp': datetime.utcnow() + timedelta(minutes=525600000),
                 'iat': datetime.utcnow(),
                 'sub': user_id
             }
